b67f76b317b1afe3f4b3cadf6e5872b25ee92ce4
Shawn DeWolfe <shawn@shawndewolfe.com>
2022-03-23 09:50:19 -0700

    Initial release

---
+++ includes/class-wb3savequeries-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://shawndewolfe.com/
+ * @since      1.0.0
+ *
+ * @package    Wb3savequeries
+ * @subpackage Wb3savequeries/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wb3savequeries
+ * @subpackage Wb3savequeries/includes
+ * @author     Shawn DeWolfe <shawn@shawndewolfe.com>
+ */
+class Wb3savequeries_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wb3savequeries-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://shawndewolfe.com/
+ * @since      1.0.0
+ *
+ * @package    Wb3savequeries
+ * @subpackage Wb3savequeries/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wb3savequeries
+ * @subpackage Wb3savequeries/includes
+ * @author     Shawn DeWolfe <shawn@shawndewolfe.com>
+ */
+class Wb3savequeries_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
